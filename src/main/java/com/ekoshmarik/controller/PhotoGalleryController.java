26f217b1674fa2c0b9b47e04e978df98444b18bd
ekoshmarik <ekoshmarik@gmail.com>
2016-11-10 15:13:44 +0200

    added mockito

--- src/main/java/com/ekoshmarik/controller/PhotoGalleryController.java
+++ src/main/java/com/ekoshmarik/controller/PhotoGalleryController.java
@@ -8,7 +8,10 @@ import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.io.ByteArrayInputStream;
--- src/main/java/com/ekoshmarik/service/PhotoGalleryService.java
+++ src/main/java/com/ekoshmarik/service/PhotoGalleryService.java
@@ -1,35 +1,90 @@
 package com.ekoshmarik.service;
 
+import com.ekoshmarik.storage.Cache;
+import com.ekoshmarik.storage.CacheImpl;
+import com.ekoshmarik.util.DirectoryScanner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 /**
- * A photo gallery service interface.
+ * The PhotoGalleryImpl implements a photo gallery service.
  */
-public interface PhotoGalleryService {
+@Service
+public class PhotoGalleryService {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(PhotoGalleryService.class);
+
+  private static final int BUFF_SIZE = 4096;
+
+  private Cache cache;
+
+  public PhotoGalleryService() {
+    this.cache = new CacheImpl();
+  }
 
   /**
    * The uploadPhotos() method scans directory for a .png image and adds its to a storage.
    * @param dir is a full path to directory to scan.
    */
-  void uploadPhotos(String dir);
+  public void uploadPhotos(String dir) {
+    LOGGER.info("Uploading photos...");
+    List<Path> files = DirectoryScanner.getFiles(dir);
+
+    if (files.isEmpty()){
+      LOGGER.info("Photos not found.");
+      return;
+    }
+
+    cache.clear();
+
+    byte[] buffer = new byte[BUFF_SIZE];
+
+    for (Path file : files) {
+      try (InputStream photo = Files.newInputStream(file)) {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+
+        while (photo.available() > 0) {
+          int size = photo.read(buffer);
+          stream.write(buffer, 0, size);
+        }
+        cache.addDataToCache(stream.toByteArray());
+      } catch (IOException e) {
+        LOGGER.error("Error occurred while reading file: {}.", e.getMessage());
+      }
+    }
+    LOGGER.info("Photos have been uploaded.");
+  }
 
   /**
    * The getPhoto() method returns a photo with a specified id from a storage.
    * @param id is a symbol which uniquely identifies an image.
    * @return a photo represented by byte array.
    */
-  byte[] getPhoto(int id);
+  public byte[] getPhoto(int id) {
+    return cache.getData(id);
+  }
 
   /**
    * The getPhotos() method returns all photos from a storage.
    * @return photos represented by byte arrays.
    */
-  List<byte[]> getPhotos();
+  public List<byte[]> getPhotos() {
+    return cache.getData();
+  }
 
   /**
    * The getAmountOfPhotos() method returns current amount of photos
    * @return current amount of photos store in a storage.
    */
-  int getAmountOfPhotos();
-
+  public int getAmountOfPhotos() {
+    return cache.getSize();
+  }
 }
